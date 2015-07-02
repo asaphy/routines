6719a2961bfed1b325f6787e5bca345aedc5efea
Asaph Yuan <asaphy@bu.edu>
2015-07-01 19:22:46 -0700

    initial commit

---
+++ Prayr/Prayr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prayr
+//
+//  Created by Asaph Yuan on 7/1/15.
+//  Copyright © 2015 Asaph Yuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
