f634b2e3fbac6d77b6cff89cedc8df0dde858bed
Asaph Yuan <asaphy@bu.edu>
2015-08-19 13:11:47 -0400

    timer working

--- Routines/Routines/RoutineViewController.swift
+++ Routines/Routines/RoutineViewController.swift
@@ -10,10 +10,29 @@ import UIKit
 
 class RoutineViewController: UIViewController {
 
+  @IBOutlet weak var habitTimeLabel: UILabel!
+  @IBOutlet weak var habitLabel: UILabel!
   @IBOutlet weak var taskCompletedButton: UIButton!
+  var timer = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "water.png")!)
+      
+      
+      struct Habit {
+        var habitName: String
+        var habitPic: String
+        var habitCategory: String
+        var habitTime: Int
+      }
+      
+      let water = Habit(habitName: "Water", habitPic: "water.png", habitCategory: "Physical", habitTime: 120)
+      self.view.backgroundColor = UIColor(patternImage: UIImage(named: water.habitPic)!)
+      self.habitLabel.text = water.habitName
+      timer = water.habitTime
+      print(timer)
+      _ = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("countdown"), userInfo: nil, repeats: true)
+
+
       taskCompletedButton.backgroundColor = UIColor.clearColor()
       taskCompletedButton.layer.cornerRadius = 10
       taskCompletedButton.layer.borderWidth = 1
@@ -26,11 +45,28 @@ class RoutineViewController: UIViewController {
     }
   
   @IBAction func cancelButton(sender: AnyObject) {
-    self.dismissViewControllerAnimated(false, completion: nil)
+    self.dismissViewControllerAnimated(true, completion: nil)
     
   }
   
   @IBAction func skipButton(sender: AnyObject) {
+  }
+  
+  func countdown() {
+    print(timer)
+    if (timer > 0){
+      timer--
+      self.habitTimeLabel.text = stringFromTimer(timer)
+    }
+    else{
+      self.habitTimeLabel.text = "Time's Up"
+    }
+  }
+  
+  func stringFromTimer(time:Int) -> String{
+    let seconds = timer % 60
+    let minutes = (timer / 60) % 60
+    return String(format: "%0.2d:%0.2d", minutes, seconds)
   }
   
 }
---
+++ Routines/Routines/routine.swift
@@ -0,0 +1,14 @@
+//
+//  routine.swift
+//  Routines
+//
+//  Created by Asaph Yuan on 8/19/15.
+//  Copyright © 2015 Asaph Yuan. All rights reserved.
+//
+
+import Foundation
+
+var routineName = "null"
+var routinePic = "null"
+var routineTime = 0.0
+var routineCategory = "null"
