7f9b7424dbc5c4fc54e6f27c5a38a1bc23522d13
Asaph Yuan <asaphy@bu.edu>
2015-08-19 10:28:44 -0400

    initial commit

---
+++ Routines/Routines/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Routines
+//
+//  Created by Asaph Yuan on 8/19/15.
+//  Copyright © 2015 Asaph Yuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    println("I can't wait to push this project to GH")
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
